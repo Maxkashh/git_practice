cab48c0e323715cee415b98cf1d699c8558642b3
Maxim Kashurin <kashurin2001@mail.ru>
2023-10-07 23:41:32 +0300

    изменил функционал на mster

--- main.py
+++ main.py
@@ -4,8 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def foo():
-    print()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
